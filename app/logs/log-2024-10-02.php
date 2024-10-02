c566b1aab8e7a5df8199d6a686e7da9ee8513420
Prakash <gautamprakash697@gmail.com>
2024-10-02 23:38:10 +0530

    Added background image ,set the ui of signout,profile user background image

---
+++ app/logs/log-2024-10-02.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-02 17:07:21 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 17:08:47 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 17:08:52 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 17:15:03 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 17:28:30 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 17:55:26 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 18:03:49 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 18:03:55 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 18:04:14 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:11:44 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:11:44 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:22:57 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:22:59 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:23:15 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:24:22 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:25:09 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:26:28 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:29:19 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:33:50 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:36:23 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:36:44 --> 404 Page Not Found: Assets/layout
+ERROR - 2024-10-02 23:36:46 --> 404 Page Not Found: Assets/layout
